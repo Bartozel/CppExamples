27cb179ebf48a82b625352105889b7c1ddf6b94c
Zdenek <zbartozel@seznam.cz>
2026-06-03 07:28:27 +0200

    Add measurement

--- CodeWars/Program.cpp
+++ CodeWars/Program.cpp
@@ -1,18 +1,36 @@
- #include <iostream>  
+ #include <iostream>
+ #include <chrono>
+ #include <stdexcept>
  #include "TextRevertor.h" 
+
+ const size_t ITERATION = 100000000;
     
+ std::chrono::duration<double> Measurement(TextRevertor& tr, std::string (TextRevertor::*m)(const std::string&));
 int main()
 {
     TextRevertor textRevertor;
-    auto result = textRevertor.reverse_words_2("This is an example!");
-    auto expected = "sihT si na !elpmaxe";
 
-    if (result != expected)
-    {
-        std::cout << "Test failed!" << std::endl;
-        return 1;
+    auto res_1 = Measurement(textRevertor, &TextRevertor::reverse_words);
+    std::cout << "result reverse_word " << res_1.count() << std::endl;
+
+    auto res_2 = Measurement(textRevertor, &TextRevertor::reverse_words_2);
+    std::cout << "result reverse_word_2 " << res_2.count() << std::endl;
+
+    return 0;
+}
+
+std::chrono::duration<double> Measurement(TextRevertor& tr, std::string (TextRevertor::*m)(const std::string&)){
+
+    auto startPoint = std::chrono::system_clock::now();
+
+    for(int i = 0; i < ITERATION; i++){
+        auto result = (tr.*m)("This is an example!");
+        if (result != "sihT si na !elpmaxe")
+        {
+            std::cout << "Test failed!" << std::endl;
+            throw std::logic_error("Invalid result");
+        }
     }
 
-    std::cout << result << std::endl;
-    return 0;
+    return std::chrono::system_clock::now() - startPoint;
 }
